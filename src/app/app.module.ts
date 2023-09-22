eed4c40ee9799d8bfe8ae46150d4117767e609d1
dchavarria <dchavarria@rentingcolombia.co>
2023-09-22 09:44:31 -0500

    local storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 
 import { SharedModule } from './shared/shared.module';
@@ -15,6 +16,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+
     SharedModule,
     GifsModule
   ],
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,19 +1,68 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { SearchResponse, Gif } from '../interfaces/gifs.interface';
 
+
+const GIPHY_API_KEY = '4cPiS7rFHknLPmQavwwi2WDW5vo5X34u'
 @Injectable({providedIn: 'root'})
 export class GifsService {
 
-  private _tagsHistory: string[]= [];
+  public gifList: Gif[] = [];
 
-  constructor() { }
+
+
+  private _tagsHistory: string[]= [];
+  private apiKey: string = '4cPiS7rFHknLPmQavwwi2WDW5vo5X34u';
+  private serviceUrl: string = 'http://api.giphy.com/v1/gifs'
+
+  constructor( private http: HttpClient) { }
 
   get tagHistory(){
     return [...this._tagsHistory];
   }
 
 
- public searchTag ( tag: string):void{
-    this._tagsHistory.unshift(tag)
+  private organizedHistory(tag:string){
+      tag = tag.toLowerCase();
+
+      if (this._tagsHistory.includes(tag)){
+        this._tagsHistory = this._tagsHistory.filter((oldtag) => oldtag !== tag)
+      }
+
+      this._tagsHistory.unshift(tag);
+
+      this._tagsHistory = this._tagsHistory.splice(0,10)
+      this.saveLocalStorage();
+  }
+
+  private saveLocalStorage():void{
+    localStorage.setItem('history', JSON.stringify (this._tagsHistory))
+  }
+
+
+ searchTag ( tag: string):void {
+
+  if (tag.length === 0) return;
+    this.organizedHistory(tag);
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', tag)
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, {params})
+    .subscribe( resp =>{
+
+      this.gifList = resp.data;
+      console.log({gifs: this.gifList});
+
+    })
+
+
+  /*   fetch('http://api.giphy.com/v1/gifs/search?api_key=4cPiS7rFHknLPmQavwwi2WDW5vo5X34u&q=valorant&limit=10')
+    .then( resp => resp.json())
+    .then(data => console.log(data)); */
+
   }
 
 
